d6eb70b8899f4f351a6fbe5835b7cf7306cb1714
Yuji181181 <s1f102402552@iniad.org>
2025-04-22 01:50:32 +0900

    定義 init

---
+++ SQL/単一テーブル/定義/CREATE.py
@@ -0,0 +1,20 @@
+import sqlite3
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+db_path = os.path.join(parent_dir, "single_table.db")
+
+
+conn = sqlite3.connect(db_path)
+cur = conn.cursor()
+
+query = "CREATE TABLE items(\
+    item_id INTEGER PRIMARY KEY,\
+    item_name STRING,\
+    price INTEGER)"
+
+cur.execute(query)
+conn.commit()
+conn.close()
