887b1aabdf076ffd18ed64c66f2971d0c2577847
Yuji181181 <s1f102402552@iniad.org>
2025-04-22 03:07:32 +0900

    編集

---
+++ SQL/単一テーブル/編集/DELETE.py
@@ -0,0 +1,17 @@
+import sqlite3
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+db_path = os.path.join(parent_dir, "single_table.db")
+
+
+conn = sqlite3.connect(db_path)
+cur = conn.cursor()
+
+query = ""
+
+cur.execute(query)
+conn.commit()
+conn.close()
